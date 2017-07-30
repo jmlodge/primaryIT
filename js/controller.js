d4581f6ec4d5c94d15fe31a2509e8f1c20d525ec
jmlodge <jm-lodge@hotmail.co.uk>
2017-07-30 19:34:32 +0100

    add fonts and templates

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,8 @@
 angular.module('RouteControllers', [])
+
     .controller('HomeController', function ($scope) {
        $scope.title = "Welcome to PrimaryIT!";
+       $scope.subtitle = "Don't Panic, We Have A Solution For You!";
     })
 
     .controller('ContactController', function ($scope) {
@@ -10,7 +12,7 @@ angular.module('RouteControllers', [])
             if ($scope.contactForm.$valid) {
                 $scope.contactContent.firstname = $scope.contact.firstname;
                 $scope.contactContent.lastname = $scope.contact.lastname;
-                $scope.contactContent.email = $sope.contact.email;
+                $scope.contactContent.email = $scope.contact.email;
             }
 
             console.log($scope.contact.firstname + " " + $scope.contact.lastname);
