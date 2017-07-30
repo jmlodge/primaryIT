b21ab92661833f41de203ec5aa68533150e4bb7a
jmlodge <jm-lodge@hotmail.co.uk>
2017-07-30 14:02:18 +0100

    add new fields in contact add background image

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,18 @@
 angular.module('RouteControllers', [])
     .controller('HomeController', function ($scope) {
        $scope.title = "Welcome to PrimaryIT!";
+    })
+
+    .controller('ContactController', function ($scope) {
+        $scope.contactContent = {};
+
+        $scope.submitForm = function () {
+            if ($scope.contactForm.$valid) {
+                $scope.contactContent.firstname = $scope.contact.firstname;
+                $scope.contactContent.lastname = $scope.contact.lastname;
+                $scope.contactContent.email = $sope.contact.email;
+            }
+
+            console.log($scope.contact.firstname + " " + $scope.contact.lastname);
+        }
     });
